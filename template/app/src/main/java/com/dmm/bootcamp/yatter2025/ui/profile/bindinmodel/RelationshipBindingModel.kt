791b67a4b242d48ee500d499640aa5a65408334e
kuro48 <kuroryu.basket@gmail.com>
2025-08-01 03:15:41 +0900

    RelationshipのBindingModelとConverter実装

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/RelationshipBindingModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/profile/bindinmodel/RelationshipBindingModel.kt
@@ -1,3 +1,7 @@
 package com.dmm.bootcamp.yatter2025.ui.profile.bindinmodel
 
-//data class RelationshipBindingModel()
+data class RelationshipBindingModel(
+    val target: String,
+    val following: Boolean,
+    val followedBy: Boolean,
+)
