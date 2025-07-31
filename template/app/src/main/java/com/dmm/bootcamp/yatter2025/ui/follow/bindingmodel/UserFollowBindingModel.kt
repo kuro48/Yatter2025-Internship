d96300b3cb54a94c9c6d48547fa1e4ed21776e48
kuro48 <kuroryu.basket@gmail.com>
2025-08-01 00:22:25 +0900

    BindingModelの作成

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/follow/bindingmodel/UserFollowBindingModel.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/follow/bindingmodel/UserFollowBindingModel.kt
@@ -1,3 +1,10 @@
 package com.dmm.bootcamp.yatter2025.ui.follow.bindingmodel
 
-data class UserFollowBindingModel()
+data class UserFollowBindingModel(
+    val id: String,
+    val displayName: String,
+    val username: String,
+    val avatar: String?,
+    val note: String,
+    val isFollowed: Boolean,
+)
