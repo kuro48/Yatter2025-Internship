4a88b363c2a7010a97b8e43caafe99059b48dde2
kuro48 <kuroryu.basket@gmail.com>
2025-08-01 00:31:19 +0900

    UIStateの作成

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/follow/UserFollowUiState.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/follow/UserFollowUiState.kt
@@ -1,3 +1,17 @@
 package com.dmm.bootcamp.yatter2025.ui.follow
 
-data class UserFollowUiState()
+import com.dmm.bootcamp.yatter2025.ui.follow.bindingmodel.UserFollowBindingModel
+
+data class UserFollowUiState(
+    val followingList: List<UserFollowBindingModel>,
+    val isLoading: Boolean,
+    val isRefreshing: Boolean
+) {
+    companion object {
+        fun empty(): UserFollowUiState = UserFollowUiState(
+            followingList = emptyList(),
+            isLoading = false,
+            isRefreshing = false,
+        )
+    }
+}
